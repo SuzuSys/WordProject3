01bc9f8ec15d4fce77ecd190eff834d556394cfc
SuzuSys <sigmatics@outlook.jp>
2022-02-28 20:33:02 +0900

    added router and store

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from 'vue';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+import vuetify from '@/plugins/vuetify';
+import { loadFonts } from '@/plugins/webfontloader';
 import Amplify from "aws-amplify";
-import awsExports from "./aws-exports";
+import awsExports from "@/aws-exports";
 import AmplifyVue from '@aws-amplify/ui-vue';
 Amplify.configure(awsExports);
 
-loadFonts()
+loadFonts();
 
 createApp(App)
+  .use(store)
+  .use(router)
   .use(vuetify)
   .use(AmplifyVue)
-  .mount('#app')
+  .mount('#app');
