805cbd65085c4516a86bc0fa84e16704e71fe3b1
SuzuSys <sigmatics@outlook.jp>
2022-03-01 14:29:20 +0900

    added bar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <template>
   <authenticator>
-    <template v-slot="{ signOut }"> <!-- user.username -->
+    <template v-slot=""> <!-- user signOut -->
       <v-app>
         <v-app-bar app>
+          <v-app-bar-nav-icon 
+            @click="drawer = true"
+          ></v-app-bar-nav-icon>
           <v-app-bar-title>
             Word Project
           </v-app-bar-title>
           <v-btn 
             class="ml-auto"
             @click="goAccount"
+            icon
           >
-            Account
-          </v-btn>
-          <v-btn 
-            @click="signOut"
-          >
-            SignOut
+            <v-icon :icon="mdiAccount" />
           </v-btn>
         </v-app-bar>
+        <v-navigation-drawer
+          v-model="drawer"
+          absolute
+          temporary
+        >
+          <v-list
+            nav
+            dense
+          >
+            <v-list-item-group
+              v-model="group"
+              active-class="text--accent-4"
+            >
+              <v-list-item>
+                <v-list-item-title>Home</v-list-item-title>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-navigation-drawer>
         <v-main>
           <router-view />
         </v-main>
@@ -27,8 +45,14 @@
 </template>
 <script>
 import '@aws-amplify/ui-vue/styles.css';
+import { mdiAccount } from '@mdi/js';
 export default {
   name: 'App',
+  data: () => ({
+    mdiAccount,
+    drawer: false,
+    group: null,
+  }),
   methods: {
     goAccount() {
       this.$router.push({ path: '/account' });
